d476c661de115db9eb488dd17a623fbfd02c09ba
Jair Victus <jairbs98@gmail.com>
2025-01-31 01:30:44 -0700

    Enhance UI with Material Design elements

--- src/constants.py
+++ src/constants.py
@@ -1,4 +1,5 @@
-# constants.py
+import os
+
 WIDTH = 800
 HEIGHT = 600
 GRID_SIZE = 20
@@ -7,8 +8,11 @@ GRID_HEIGHT = HEIGHT // GRID_SIZE
 
 WHITE = (255, 255, 255)
 GREEN = (76, 175, 80)  # Material Design Green
-RED = (244, 67, 54)    # Material Design Red
-BLACK = (33, 33, 33)   # Material Design Dark
+RED = (244, 67, 54)  # Material Design Red
+BLACK = (33, 33, 33)  # Material Design Dark
 BLUE = (33, 150, 243)  # Material Design Blue
-FONT_PATH = "src/fonts/Roboto-Regular.ttf"  # Ruta a la fuente Roboto
+
+# Ruta a la fuente Roboto
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+FONT_PATH = os.path.join(BASE_DIR, "fonts", "Roboto-Regular.ttf")
 FONT_SIZE = 24
